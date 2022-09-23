3a7dd69b66f3fed0ac75f3ba636bc1fb243d89e0
Aslekiel <101809691+Aslekiel@users.noreply.github.com>
2022-09-23 18:11:34 +0300

    refactoring code

--- MyProject/src/controllers/books/getFilteredArrayOfBooks.ts
+++ MyProject/src/controllers/books/getFilteredArrayOfBooks.ts
@@ -5,29 +5,9 @@ export const getFilteredArrayOfBooks: Handler = async (req, res, next) => {
   try {
     const filters = req.body;
 
-    for (let i = 0; i < filters.length; i++) {
-      // eslint-disable-next-line no-await-in-loop
-      // const books = await db.books.find({
-      //   relations: { genres: true },
-      //   where: { genres: { name: filters[i] } },
-      // });
-
-      // eslint-disable-next-line no-await-in-loop
-      // const books = await db.books.createQueryBuilder('books')
-      //   .leftJoinAndSelect('books.genres', 'genres')
-      //   .where('genres.name = :name', { name: filters[i] }).getMany();
-
-      // // eslint-disable-next-line no-console
-      // console.log(books);
-
-      // res.json({ books });
-    }
-
     const books = await db.books.createQueryBuilder('books')
-      .innerJoin('books.genres', 'genres', 'genres.name IN (:...filters)', { filters }).getMany();
-
-    // eslint-disable-next-line no-console
-    console.log(books);
+      .innerJoinAndSelect('books.genres', 'genres', 'genres.name IN (:...filters)', { filters })
+      .getMany();
 
     res.json({ books });
   } catch (error) {
